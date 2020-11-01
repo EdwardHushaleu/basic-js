c01392a1a192e04cf615ea33f737ac04765a9b8e
EdwardHushaleu <2779522@mail.com>
2020-11-01 11:04:09 +0300

    feat: Part of tasks done

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,6 +1,17 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function countCats(/* matrix */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function countCats(backyard) {
+  let result = 0;
+
+  backyard.forEach((element) => {
+    element.forEach(function(el){
+      if(el === '^^'){
+      return  result++
+      } else {
+      return  null
+      }
+    })
+  });
+
+  return result;
 };
--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,12 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if(Array.isArray(members)){
+    let res  = members.filter((i) => typeof i === 'string');
+    let res1 = res.map(i => i.trim().toUpperCase());
+    let res2 = res1.map(i => i.slice(0, 1)).sort().join(''); 
+    return res2
+   }else{
+     return false
+   }
 };
